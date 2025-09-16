aa1e44f8a30fbf7209f3f45652fa398db2f827f2
Roman <dersc@ROMAN-GALAXY>
2025-09-15 23:22:24 -0400

    Added Program 2!!

--- IDE and VCS Workflow Project.cpp
+++ IDE and VCS Workflow Project.cpp
@@ -1,9 +1,19 @@
 // IDE and VCS Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// New version that displays name and credentials
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name, address, phoneNumber;
+	// Storing (mostly) bogus info here
+	name = "Roman";
+	address = "1234 Main St, Anytown, USA";
+	phoneNumber = "555-123-4567";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phoneNumber << endl;
+	return 0;
+   
 }
